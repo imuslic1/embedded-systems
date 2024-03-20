db0dd6dc2cb471f4db35951cdbab5e34412f8c71
Ismar Muslić <ismar.muslic@outlook.com>
2024-03-20 14:23:45 +0100

    updated lv2-z1 + lv2 report

--- US/LV/lv2/z1/main.cpp
+++ US/LV/lv2/z1/main.cpp
@@ -1,46 +1,19 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 #include "lpc1114etf.h"
 
 DigitalOut E(LED_ACT);
-
-DigitalOut myled1(LED0);
-DigitalOut myled2(LED1);
-DigitalOut myled3(LED2);
-DigitalOut myled4(LED3);
-DigitalOut myled5(LED4);
-DigitalOut myled6(LED5);
-DigitalOut myled7(LED6);
-DigitalOut myled8(LED7);
+BusOut leds(LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7);
 
 int main() {
     E = 0;
-    myled1 = myled2 = myled3 = myled4 = myled5 = myled6 = myled7 = myled8 = 0;
+    leds=0;
     
     while(1) {
-        myled1 = 1;
-        wait_us(1e6);
-        myled1 = 0;
-        wait_us(1e6);
-
-        myled2 = 1;
-        wait_us(1e6);
-        myled2 = 0;
-        wait_us(1e6);
-
-        myled3 = 1;
-        wait_us(1e6);
-        myled3 = 0;
-        wait_us(1e6);
-
-        myled4 = 1;
-        wait_us(1e6);
-        myled4 = 0;
-        wait_us(1e6);
-
+        for(int i=1; i<16; i*=2){
+            leds=i;
+            wait_us(1e6);
+            leds = 0;
+            wait_us(1e6);
+        }
     }
 }
